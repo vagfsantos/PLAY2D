3f91f85cf60b903f78a5524617e7907d9cde2d2a
Vagner Ferreira Santos <cl1470@fiap.com.br>
2017-10-23 16:46:11 -0200

    Documenting World class

--- src/js/main/features/world/World.feature.ts
+++ src/js/main/features/world/World.feature.ts
@@ -1,21 +1,46 @@
 import { Scene, GameLoop, Canvas } from "../_exports";
 
+/*
+* World
+* The placeholder for the game
+*/
 export class World {
+
+  /*
+  * It holds all scenes of the game
+  */
   private scenes: Scene[] = [];
+
+  /*
+  * It holds the current frame rate
+  */
   private _fps = 30;
+
+  /*
+  * They hold current and previous time, to render the scenes properly
+  */
   private _currentTime: number = 0;
   private _previousTime: number = 0;
 
+  /*
+  * Recive the id of the world and the canvas to paint into
+  */
   constructor(private _id: string, private _canvas: Canvas) {
 
   }
 
+  /*
+  * It Adds scenes to the world
+  */
   add(scene: Scene): World {
 
     this.scenes.push(scene);
     return this;
   }
 
+  /*
+  * It handle the rendering of the world
+  */
   render(): void {
     this._currentTime = new Date().getTime();
     let elapsed = this._currentTime - this._previousTime;
@@ -33,6 +58,9 @@ export class World {
     this.initLoop();
   }
 
+  /*
+  * Init the loop game
+  */
   private initLoop() {
 
     GameLoop.start(this);
