38cf1261f281ca87af422616619dac874f9eaea8
Vagner Ferreira Santos <cl1470@fiap.com.br>
2017-10-23 16:25:39 -0200

    Documenting Gravity class

--- src/js/main/features/gravity/Gravity.feature.ts
+++ src/js/main/features/gravity/Gravity.feature.ts
@@ -1,12 +1,24 @@
+/*
+* Gravity
+* It handles gravity rules for objects
+*/
 export class Gravity {
-
+  /*
+  * It holds the acceleration default for the instance
+  */
   private _acceleration = 0.10;
 
+  /*
+  * Return the acceleration for the current instance
+  */
   getAcceleration(): number {
 
     return this._acceleration;
   }
 
+  /*
+  * return the method rule to apply the fall gravity to any object
+  */
   fall(): Function {
     let acceleration = this._acceleration;
     return function() {
