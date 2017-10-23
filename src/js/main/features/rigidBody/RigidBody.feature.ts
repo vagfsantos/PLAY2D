ba4f7e65152d24e3656f392078654b5719ec0e91
Vagner Ferreira Santos <cl1470@fiap.com.br>
2017-10-23 16:35:35 -0200

    Documenting Rigid Body class

--- src/js/main/features/rigidBody/RigidBody.feature.ts
+++ src/js/main/features/rigidBody/RigidBody.feature.ts
@@ -1,24 +1,47 @@
 import { $ColisableArea, Canvas, GameObject } from "../_exports";
 
+/*
+* RigidBody
+* It can turn an object into a rigid body
+*/
 export class RigidBody {
 
+  /*
+  * It holds all colisable areas that can be in an object
+  */
   private _colisableAreas: $ColisableArea[][] = [];
+
+  /*
+  * It holds the game object it self
+  */
   private _gameObjectInfo: GameObject
 
+  /*
+  * Require the game object that can become a rigid body
+  */
   constructor(gameObject: GameObject ) {
     this._gameObjectInfo = gameObject;
   }
 
+  /*
+  * Setter for colider data
+  */
   setColisableArea( colisablePros: $ColisableArea[] ) {
 
     this._colisableAreas.push(colisablePros);
   }
 
+  /*
+  * Getter for colider data
+  */
   getColisableAreas(): $ColisableArea[][] {
 
     return [].concat( this._colisableAreas );
   }
 
+  /*
+  * It render the colider areas to degub easily
+  */
   renderOnMarkee() {
 
     for( let areasArray of this._colisableAreas ) {
