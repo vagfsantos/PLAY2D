5d63c666b1010d5531930ff10e3a13da6e4e17b9
Vagner Ferreira Santos <vagner.santos@fiap.com.br>
2017-12-07 14:21:12 -0200

    configuring the development enviroment

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+class Test {
+    constructor() {
+        this.test = 1;
+    }
+    log() {
+        console.log(this.test);
+    }
+}
+//# sourceMappingURL=index.js.map
---
+++ src/test.ts
@@ -0,0 +1,9 @@
+class Test
+{
+    test: number = 1;
+
+    log()
+    {
+        console.log(this.test);
+    }
+}
