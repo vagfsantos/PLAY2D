9852c9a958a295bf5dbedc7ee66bf519a3e0d942
Vagner Ferreira Santos <vagner.santos@fiap.com.br>
2018-03-01 15:09:18 -0300

    Creating World class

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import { Game } from './src/Game/Game'
 import { Assets } from './src/Assets/Assets';
+import { World } from './src/World/World';
+import { Scene } from './src/Scene/Scene';
 
-export { Game, Assets }
+export { Game, Assets, World, Scene }
---
+++ src/World/World.ts
@@ -0,0 +1,75 @@
+import { Scene } from "../Scene/Scene";
+
+/**
+ * World
+ * A placeholder for save scenes and organize the user ideias of the game
+ */
+export class World {
+
+  /**
+   * The name of the world
+   */
+  __name: string
+
+  /**
+   * Current Scene being rendered, used as index of an array
+   */
+  __currentScene = 0
+
+  /**
+   * Array of Scenes objects that belogns to this world
+   */
+  __scenes: Scene[] = []
+
+  constructor(name: string) {
+
+    this.__name = name
+  }
+
+  /**
+   * Returns the current name of the world
+   */
+  getName() {
+
+    return this.__name
+  }
+
+
+  /**
+   * Adds new Scenes to the world
+   */
+  addScene(scene: Scene) {
+
+    this.__scenes.push(scene)
+  }
+
+  /**
+   * Goes to a especfic Scene number if exists
+   */
+  goToScene(sceneNumber: number) {
+
+    if( this.__scenes[sceneNumber] ) {
+
+      this.__currentScene = sceneNumber
+    } else {
+
+      throw new Error(`Scene number ${sceneNumber} do not exist in this World: ${this.getName()}`)
+    }
+  }
+
+
+  /**
+   * Handler the rendering calls of the scenes
+   */
+  init() {
+
+    let currentScene = this.__scenes[this.__currentScene]
+    if( currentScene ) {
+
+      currentScene.render()
+    } else {
+
+      throw new Error(`Make sure to add a new Scene to your world before initialize it.`)
+    }
+  }
+}
