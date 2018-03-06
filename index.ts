ee86c9a961de1d135b2cb2f56d33d96f7fa10549
Vagner Ferreira Santos <vagner.santos@fiap.com.br>
2018-03-06 17:07:00 -0300

    Events - Basic usage of keyboard events

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ import { Assets } from './src/Assets/Assets';
 import { World } from './src/World/World';
 import { Scene } from './src/Scene/Scene';
 import { GameObject } from './src/GameObject/GameObject';
+import { Events } from './src/Events/Events';
 
-export { Game, Assets, World, Scene, GameObject}
+export { Game, Assets, World, Scene, GameObject, Events}
