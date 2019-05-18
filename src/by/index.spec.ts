606fe0a569c26d7a7508997aed6ba4b1cf657430
Vagner Santos <santos.vagnerferreira@gmail.com>
2019-05-18 14:45:36 -0300

    Implements by function

---
+++ src/by/index.spec.ts
@@ -0,0 +1,6 @@
+import { by } from "./index";
+
+describe("By | Function", () => {
+  it("returns a valid Vector2D with x and y property", () =>
+    expect(by(300, 200)).toMatchObject({ x: 300, y: 200 }));
+});
---
+++ src/by/index.ts
@@ -0,0 +1,5 @@
+import { at } from "../at";
+
+export function by(x: number, y: number): Vector2d {
+  return at(x, y);
+}
